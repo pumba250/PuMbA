c73646be405c6ad60cf7f8108fd2c04b79f317d1
pumba250 <158454148+pumba250@users.noreply.github.com>
2024-02-02 13:08:17 +0300

    Update top.php

--- incl/top.php
+++ incl/top.php
@@ -5,19 +5,17 @@
 <?php
 echo $PAGETITLE.$_SERVER['SERVER_NAME']; echo '</title>';
 echo '<link rel="canonical" href="https://'. $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'].'"/>';
-$logo = '<img src="/img/logo.png" alt="logo" width="55" height="55" />';
+$logo = '';
 
 echo '
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <meta name="Generator" content="'.$version.'" />
 <meta name="robots" content="index, follow">
-<link rel="icon" href="/favicon.ico" type="image/x-icon">
 <meta name="language" content="Russian">
 <meta name="revisit-after" content="2 days">
-<meta name="author" content="pumba">
 ';
 
-echo"<meta name='keywords' content='{$keyword}'></head>";
+echo "<meta name='keywords' content='{$keyword}'></head>";
 echo '<body>
 <div id="mainwrapper">
   <header> 
