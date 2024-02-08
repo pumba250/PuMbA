91373012be0076afdd14c7be683c0f17820b586c
pumba250 <158454148+pumba250@users.noreply.github.com>
2024-02-08 11:50:53 +0300

    Update index.php

--- index.php
+++ index.php
@@ -9,36 +9,13 @@ include("incl/boot.php");
 if ($maintance>0){
 	echo '<center><img src="/img/maintance.jpg" height="70%">';
 } else {
-
-//gzip();
-$user = null;
-if (check_auth()) {
-    // Получим данные пользователя по сохранённому идентификатору
-    $stmt = pdo()->prepare("SELECT * FROM `{$usertab}` WHERE `id` = :id");
-    $stmt->execute(['id' => $_SESSION['user_id']]);
-    $user = $stmt->fetch(PDO::FETCH_ASSOC);
-}
-if ($user) {
-$userBirthday = $user['birthday']; // День рождение юзера
-$birthday = strtotime($userBirthday); // Получаем unix timestamp нашего дня рождения
-$years = date('Y') - date('Y',$birthday); // Вычисляем возраст БЕЗ учета текущего месяца и дня
-$now = time(); // no comments
-$nowBirthday = mktime(0,0,0,date('m',$birthday),date('d',$birthday),date('Y')); // Получаем день рождение пользователя в этом году
-if ($nowBirthday > $now) {
-    $years --; // Если дня рождения ещё не было то вычитаем один год
-}
-//echo $years;
-$_SESSION['valid_adult']=$years;
-}
-if (!isset($_GET['mod']) || ($_GET['mod']=="") || (!file_exists ("mods/".$_GET['mod'].".php"))) {
+if (!isset($_GET['mod']) || ($_GET['mod']=="")) {
 	$_GET['mod'] = $sys_def_mod;
 }
-if (!file_exists("mods/".@$_GET['mod'].".php")) {
-	include('mods/404.php');
+if (!file_exists("module/".@$_GET['mod'].".php")) {
+	include('module/404.php');
 	} else {
-@include("mods/" . $_GET['mod'] . ".php");
-// gzdocout(); 
+@include("module/" . $_GET['mod'] . ".php");
  }
 }
-
 ?>
